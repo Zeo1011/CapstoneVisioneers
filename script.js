49512bd2e8f18519b5df39b211f6f7ad36765b9a
Zeo1011 <130911451+Zeo1011@users.noreply.github.com>
2024-04-23 02:34:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const nav = document.querySelector('.navBar');
+    const menuIcon = document.querySelector('.menu');
+
+    menuIcon.addEventListener('click', function() {
+        nav.classList.toggle('navBar2');
+    });
+});
+
+
+
+
+
